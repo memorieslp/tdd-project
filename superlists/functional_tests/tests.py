a21a9736e7b956a9e71102dae65cd5e2e6c642f4
memorieslp <42478443+memorieslp@users.noreply.github.com>
2020-09-07 16:14:48 -0300

    Trabalhando com Multiplas Listas e API REST

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -13,7 +13,7 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
 
         # Rapha decidiu utilizar o novo app TODO. Ele entra em sua pagina principal:
         self.browser.get(self.live_server_url)
@@ -64,3 +64,43 @@ class NewVisitorTest(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Maria começa uma nova lista
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Estudar testes funcionais')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+
+        # Ela nota que sua lista possui uma URL unica
+        maria_list_url = self.browser.current_url
+        self.assertRegex(maria_list_url, '/lists/.+')
+
+        # Agora, um novo usuario, Joao, entra no site
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Joao visita a pagina inicial. Nao existe nenhum sinal da lista de Maria
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('1: Estudar testes funcionais', page_text)
+        self.assertNotIn('2: Estudar testes de unidade', page_text)
+
+        # Joao inicia uma nova lista
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Comprar leite')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Comprar leite')
+
+        # Joao pega sua URL unica
+        joao_list_url = self.browser.current_url
+        self.assertRegex(joao_list_url, '/lists/.+')
+        self.assertNotEqual(joao_list_url, maria_list_url)
+
+        # Novamente, nao existe sinal da lista de Maria
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Estudar testes funcionais', page_text)
+        self.assertIn('Comprar leite', page_text)
+
+        # Satisfeitos, ambos vao dormir
